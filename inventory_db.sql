d4f115d0290072a38cbfe333efea684a75cc3278
Kyrushui <devkyrushui@gmail.com>
2025-10-23 14:49:58 -0600

    added Employee, Branch, Supermarket Company, and Supplier tables to the sql db.

--- inventory_db.sql
+++ inventory_db.sql
@@ -1,1 +1,24 @@
 -- SQL Code here
+CREATE TABLE EMPLOYEE (
+    PRIMARY KEY (employee_id),
+
+    emp_name    VARCHAR(100)        NOT NULL,
+    email       VARCHAR(320)        NOT NULL,
+    job_type    VARCHAR(20)         NOT NULL,
+    branch_code INT                 NOT NULL,
+);
+
+CREATE TABLE BRANCH (
+    PRIMARY KEY (branch_code),
+
+    branch_location VARCHAR(MAX)    NOT NULL,
+    company_name    VARCHAR(100)    NOT NULL,
+);
+
+CREATE TABLE SUPERMARKET_COMPANY (
+    PRIMARY KEY (company_name)
+);
+
+CREATE TABLE SUPPLIER (
+    PRIMARY KEY (supplier_name)
+);
