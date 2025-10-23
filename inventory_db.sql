f9671021bd5736806ddaf075ffedfa0cb8dad017
Kyrushui <devkyrushui@gmail.com>
2025-10-23 14:54:54 -0600

    Fixes to additions

--- inventory_db.sql
+++ inventory_db.sql
@@ -1,24 +1,31 @@
 -- SQL Code here
 CREATE TABLE EMPLOYEE (
-    PRIMARY KEY (employee_id),
-
+    employee_id BIGINT              NOT NULL,
     emp_name    VARCHAR(100)        NOT NULL,
     email       VARCHAR(320)        NOT NULL,
     job_type    VARCHAR(20)         NOT NULL,
     branch_code INT                 NOT NULL,
-);
 
-CREATE TABLE BRANCH (
-    PRIMARY KEY (branch_code),
-
-    branch_location VARCHAR(MAX)    NOT NULL,
-    company_name    VARCHAR(100)    NOT NULL,
+    PRIMARY KEY (employee_id)
 );
 
 CREATE TABLE SUPERMARKET_COMPANY (
+    company_name    VARCHAR(100)    NOT NULL,
+    
     PRIMARY KEY (company_name)
 );
 
+CREATE TABLE BRANCH (
+    branch_code     BIGINT          NOT NULL,
+    branch_location VARCHAR(MAX)    NOT NULL,
+    company_name    VARCHAR(100)    NOT NULL,
+
+    FOREIGN KEY (company_name) REFERENCES SUPERMARKET_COMPANY(company_name)
+    PRIMARY KEY (branch_code)
+);
+
 CREATE TABLE SUPPLIER (
+    supplier_name   VARCHAR(100)    NOT NULL,
+
     PRIMARY KEY (supplier_name)
 );
