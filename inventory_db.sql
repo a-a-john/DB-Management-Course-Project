bee53f5097f850183de9c95410ef4d57f6c2261b
Kyrushui <devkyrushui@gmail.com>
2025-10-24 15:23:13 -0600

    fixed formatting, and changed table name from CONTAINS_ -> ORDER_CONTAINS

--- inventory_db.sql
+++ inventory_db.sql
@@ -5,35 +5,39 @@ CREATE DATABASE inventory_db;
 USE inventory_db;
 
 CREATE TABLE ORDER_PLACEMENT (
-	BranchNo SMALLINT NOT NULL,
-    SupplierName VARCHAR(25) NOT NULL,
-    OrderNo INT AUTO_INCREMENT NOT NULL,
-    FOREIGN KEY (BranchNo) REFERENCES BRANCH(BranchCode),
-    FOREIGN KEY (SupplierName) REFERENCES SUPPLIER(SupplierName),
-    FOREIGN KEY (OrderNo) REFERENCES ORDER_(OrderNo),
+	branch_no       SMALLINT            NOT NULL,
+    supplier_name   VARCHAR(25)         NOT NULL,
+    order_no        INT AUTO_INCREMENT  NOT NULL,
+
+    FOREIGN KEY (branch_no) REFERENCES BRANCH(branch_no),
+    FOREIGN KEY (supplier_name) REFERENCES SUPPLIER(supplier_no),
+    FOREIGN KEY (order_no) REFERENCES ORDER_(order_no)
     PRIMARY KEY (BranchNo,SupplierName,OrderNo));
     
-CREATE TABLE CONTAINS_ (
-	OrderNo INT AUTO_INCREMENT NOT NULL,
-    ItemCode SMALLINT NOT NULL,
-    Quantity TINYINT NOT NULL,
-    FOREIGN KEY (OrderNo) REFERENCES ORDER_(OrderNo),
-    FOREIGN KEY (ItemCode) REFERENCES ITEM(ItemCode),
-    PRIMARY KEY (OrderNo, ItemCode));
+CREATE TABLE ORDER_CONTAINS (
+	order_no    INT AUTO_INCREMENT  NOT NULL,
+    item_code   SMALLINT            NOT NULL,
+    quantity    SMALLINT            NOT NULL,
+
+    FOREIGN KEY (order_no) REFERENCES ORDER_(order_no),
+    FOREIGN KEY (item_code) REFERENCES ITEM(item_code),
+    PRIMARY KEY (order_no, item_code));
     
 CREATE TABLE OFFERS (
-	BranchNo SMALLINT NOT NULL,
-    ItemCode SMALLINT NOT NULL,
-    FOREIGN KEY (BranchNo) REFERENCES BRANCH(BranchCode),
-    FOREIGN KEY (ItemCode) REFERENCES ITEM(ItemCode),
-    PRIMARY KEY (BranchNo, ItemCode));
+	branch_no    SMALLINT    NOT NULL,
+    item_code    SMALLINT    NOT NULL,
+
+    FOREIGN KEY (branch_no) REFERENCES BRANCH(branch_code),
+    FOREIGN KEY (item_code) REFERENCES ITEM(item_code),
+    PRIMARY KEY (branch_no, item_code));
     
 CREATE TABLE BELONGS_TO (
-	PurchaseNo SMALLINT NOT NULL,
-    ItemCode SMALLINT NOT NULL,
-    FOREIGN KEY (PurchaseNo) REFERENCES PURCHASE(PurchaseNo),
-    FOREIGN KEY (ItemCode) REFERENCES ITEM(ItemCode),
-    PRIMARY KEY (PurchaseNo, ItemCode));
+	purchase_no SMALLINT    NOT NULL,
+    item_code   SMALLINT    NOT NULL,
+
+    FOREIGN KEY (purchase_no) REFERENCES PURCHASE(purchase_no),
+    FOREIGN KEY (item_code) REFERENCES ITEM(item_code),
+    PRIMARY KEY (purchase_no, item_code));
 	
     
     
